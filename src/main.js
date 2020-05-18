29ab072a15da452c7d1fa8612ff75e613d58a5f1
olafk <olaf.koziara@gmail.com>
2020-05-18 16:26:22 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App,{
+  props:{
+    endpoint:"http://code.eduweb.pl/kurs-vue/images/?wait=1"
+  }
+})
 }).$mount('#app')
