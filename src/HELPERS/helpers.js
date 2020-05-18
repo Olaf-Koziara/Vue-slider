f4c21cd0ff11fbc4e5c1fc360e9696e89a3435a1
olafk <olaf.koziara@gmail.com>
2020-05-18 16:26:17 +0200

    Update helpers.js

--- src/HELPERS/helpers.js
+++ src/HELPERS/helpers.js
@@ -2,8 +2,8 @@ export function preloadImage(url)
 {
     const img = document.createElement("img");
     const p  = new Promise(function(resolve,reject){
-        img.onload= () => resolve(url);
-        img.onerror = () => reject(url);
+        img.onload= () => resolve();
+        img.onerror = () => reject();
     }
     );
     img.src=url;
