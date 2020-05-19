4ce57fb2d4d05265cc38b90cf8bdf9ab9cb149c2
olafk <olaf.koziara@gmail.com>
2020-05-19 14:05:19 +0200

    Update main.js
    routing

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from "./routes/routes"
 
 Vue.config.productionTip = false
-
+const router = new VueRouter({
+  routes
+})
+Vue.use(VueRouter);
 new Vue({
+  el:'#app',
+  router,
   render: h => h(App,{
   props:{
-    endpoint:"http://code.eduweb.pl/kurs-vue/images/?wait=1"
+    endpoint:"http://code.eduweb.pl/kurs-vue/images/"
   }
 })
 }).$mount('#app')
